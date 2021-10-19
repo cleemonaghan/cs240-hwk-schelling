f1f99b95f53a4f1e534c0d8bc015a7e136efe96d
Colin Monaghan <cleemonaghan@gmail.com>
2021-10-19 15:43:42 -0700

    Created updateBoard() function in schelling.js to retrieve inputs from the html

--- schelling.js
+++ schelling.js
@@ -1,4 +1,28 @@
 /**
  * Schelling's Model simulator
- * @author
+ * @author Colin Monaghan
  */
+
+//initialize the board
+updateBoard();
+
+/**
+ * This method gathers the inputs and generates the board
+ */
+function updateBoard() {
+	//get the inputed size of the board
+	let dimension = document.querySelector("#dimension").value;
+	//get color1 from the inputs
+	let color1 = document.querySelector("#popXcolor").value;
+	//get color2 from the inputs
+	let color2 = document.querySelector("#popYcolor").value;
+	//get the % population split from the inputs
+	let popRatio = document.querySelector("#popRatio").value;
+	//get the % vacant cells from the inputs
+	let vacantRatio = document.querySelector("#vacantRatio").value;
+
+	//generate the board
+	generateBoard(dimension, color1, color2, popRatio, vacantRatio);
+}
+
+function generateBoard(dimension, color1, color2, popRatio, vacantRatio) {}
